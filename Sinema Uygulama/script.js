9fe006cbe645e6d07ef8c326cf53f21a8cb912f9
gmzlmdr <gamzelmdr@gmail.com>
2022-12-01 22:58:27 +0300

    js kısmı

--- Sinema Uygulama/script.js
+++ Sinema Uygulama/script.js
@@ -1,7 +1,7 @@
-const container = document.querySelector('.container');
+const container = document.querySelector('.container');  //seçtiğimiz eleman container classına sahip
 const count = document.getElementById('count');
 const amount = document.getElementById('amount');
-const selected = document.getElementById('movie');
+const select = document.getElementById('movie');
 const seats = document.querySelectorAll('.seat:not(.reserved)');
 
 container.addEventListener('click',function(e){
@@ -11,7 +11,6 @@ container.addEventListener('click',function(e){
         //console.log(e.target); // seat classına sahip olan eleman geliyor.
         e.target.classList.toggle('selected');// true ise false , false ise true yapıyor. seçilen elemana selected classı koyuyor veya kaldırıyor.
         calculatorTotal();
-
     } 
 });
 
@@ -24,21 +23,46 @@ function calculatorTotal(){
         const seatsArr = [];
 
         selectedSeats.forEach(function(seat){
-            selectedSeatsArr.push(seats);
+            selectedSeatsArr.push(seat);
         });
 
         //spread 
         seats.forEach(function(seat){
-            seatsArr.push(push);
+            seatsArr.push(seat);
         })
-
+        //[1,3,5]
         let selectedSeatIndexs = selectedSeatsArr.map(function(seat){
-            
+            return seatsArr.indexOf(seat);
         })
+        console.log(selectedSeatIndexs);
         //console.log(seats);
         //console.log(selectedSeats);
         let selectedSeatCound = container.querySelectorAll('.seat.selected').length;// seçilen elemanların sayısını alıyoruz.
         //console.log(selectedSeatCound); 
         count.innerText = selectedSeatCound; // seçilen element sayısını yazdır.
-        amount.innerText = selectedSeatCound * selected.value; // seçilen eleman sayılarını fiyatlarını hesapla.
-}
+        amount.innerText = selectedSeatCound * select.value; // seçilen eleman sayılarını fiyatlarını hesapla.
+        
+        saveToLocalStorage(selectedSeatIndexs);
+    }
+
+    function getFromLocalStorage(){
+        const selectedSeats = JSON.parse(localStorage.getItem('selectedSeats'));
+
+        if (selectedSeats != null && selectedSeats > 0 ){
+            seats.foreach(function(seat,index) {
+                if(selectedSeats.indexOf(index) > -1 ){
+                    seat.classList.add('selected');
+                }
+            })
+        }
+
+        const selectedMovieIndex = localStorage.getItem('selectedMovieIndex');
+
+        if (selectedMovieIndex != null){
+            select.selectedIndex = selectedMovieIndex;
+        }
+    }
+    function saveToLocalStorage(indexs){
+        localStorage.setItem('selectedSeats',JSON.stringify(indexs));
+        localStorage.setItem('selectedMovieIndex',select.selectedIndex);
+    }
